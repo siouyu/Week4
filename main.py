2c814ac5d55a0f6269326e51f1558315e7e012d8
Siouyu Hsu <46859822+siouyu@users.noreply.github.com>
2023-07-29 00:29:53 +0800

    session 版本

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from flask import *  # 米字號是 import 全部功能、理論上是輸入需要的功能名
-from cryptography.fernet import Fernet
 
 app = Flask(__name__) # 建立 application 物件。app 是變數名稱，可自行更換
 app.secret_key="secret"
-key = Fernet.generate_key()
-f = Fernet(key)
 
 # Home page
 @app.route("/", methods=["GET","POST"]) 
 def home(): 
-    if request.cookies.get("username"):
+    if session.get("username"):
         return redirect("/member")
     else:
         return render_template("home.html")
@@ -23,11 +20,9 @@ def signin():
         if not username or not password:
             return redirect("/error?message=請輸入帳號和密碼")
         elif username == "test" and password == "test":
-            byteUsername = str.encode(username)
-            cipher = f.encrypt(byteUsername)
-            resp = make_response(redirect("/member"))
-            resp.set_cookie("username", cipher)
-            return resp
+            session["username"] = username
+            session["password"] = password
+            return redirect("/member")
         else:
             return redirect("/error?message=帳號或密碼輸入錯誤")
     else:
@@ -36,7 +31,7 @@ def signin():
 # 會員頁
 @app.route("/member", methods=["GET","POST"])
 def member():
-    if request.cookies.get("username"):
+    if session.get("username"):
         return render_template("member.html")
     else:
         return redirect(url_for("home"))
@@ -50,10 +45,8 @@ def error():
 # User state management
 @app.route("/signout")
 def signout():
-    res = make_response(redirect("/"))
-    res.delete_cookie("username")
-    return res
-
+    session.clear()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(port=3000, debug = True)
